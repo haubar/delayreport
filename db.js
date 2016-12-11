0746fde775562c6a964e2a3cccb8dd53703259e5
haubar <haubarclack@gmail.com>
2016-12-11 21:49:13 +0800

    update ettoday

--- db.js
+++ db.js
@@ -9,5 +9,14 @@ var news = mongoose.model('news', {
                     created_at: String,
                     updated_at: Date
               });
+var test = mongoose.model('test', {
+                    title: String,
+                    link: String,
+                    category: String,
+                    type: String,
+                    created_at: String,
+                    updated_at: Date
+              });              
 mongoose.model( 'news', news );
+mongoose.model( 'test', test );
 mongoose.connect( process.env.DB_HOST );
--- et.js
+++ et.js
@@ -2,60 +2,92 @@
 require( './db' );
 var mongoose = require('mongoose');
 var news     = mongoose.model('news');
-
+var fs = require('fs');
 var Crawler = require('crawler2');
 var jsdom = require('jsdom');
 var moment = require('moment');
 
 var newsurl = 'http://www.ettoday.net/'
-var newsdate = 20030505;
+var startd = moment(process.argv[2]);
+var endd = moment(process.argv[3]); //moment();
+var newsdate = startd;
 var datenow = moment().format("YYYY-MM-DD");
 var datecreate = moment(newsdate).format("YYYYMMDD");
 var cate = 'none';
 
 var craw = new Crawler({
-		maxConnections : 10,
+		maxConnections : 8,
 		jQuery : jsdom,
 		forceUTF8 : true,
 		callback : function (error, result, $){
-				var tolink, title;
-				$('.wrapper_box .container_box .block_z1:first a').each(function(index, a) {
-				 tolink = $(this).attr('href');
+				var tolink, title, created;
+			
+				if(!!$('#all-news-list h3'))
+				$('#all-news-list h3').each(function(index, a) {
+				 tolink = $(this).find('a').attr('href');
 				 totitle = $(this).text();
-			   var historyData = new news({
-			   title: totitle,
-			   link: newsurl+tolink,
-			   category: cate,
-			   type: 'ettoday',
-			   created_at: newsdate,
-         	   updated_at: datenow
-			 });
+				 created = tolink.replace('http://www.ettoday.net/news/', "")
+				 created = created.replace(/[/]\d+(.htm)/ig, '')
+			    var historyData = new news({
+			    title: totitle,
+			    link: tolink,
+			    category: cate,
+			    type: 'ettoday',
+			    created_at: created,
+         	    updated_at: datenow
+			   });
+			  console.log(created);
+			  console.log(tolink);
+			   console.log(totitle);
+	
 			 historyData.save(function (err) {
 			   if (err)
 			   console.log(tolink);
 			   console.log(totitle);
 			 });
      	});
-		 pagecraw.queue(tolink);
-		}
+		 
+	}
 });
 
+//爬分頁，爬不到，先放著(?)
+/*
 var pagecraw = new Crawler({
-	maxConnections : 10,
+	maxConnections : 8,
 	jQuery : jsdom,
 	callback : function (error, result, $){
-		if(!!$('#all-news-list'))
-			$('#all-news-list h3').each(function(index, a) {
-			 var tolink = $(this).find('a').attr('href');
-			 var totitle = $(this).text();
- 	});
+		var pagelink, pagetitle;
+		// if(!!$('div.menu_page'))
+			// $('#all-news-list h3').each(function(index, a) {
+		$('.menu_page').each(function(index, a) {
+			console.log($(this).html);
+			 var pagelink = $(this).attr('href');
+			 var pagetitle = $(this).text();
+ 	    });
+		// console.log($(this).html);
+		// console.log(pagelink);
+		// console.log(pagetitle); 
  	}
+
 });
+*/
 
 var customSearch = function(keyword){
-	return 'http://www.ettoday.net/' + keyword;
+	return 'http://www.ettoday.net/news/news-list-'+ keyword +'-0-'+ num +'.htm';
 };
 
-craw.queue({
-  uri: customSearch('sitemap.htm')
-});
+
+var centerday;
+for (var m = moment(startd); m.diff(endd, 'days') <= 0; m.add(1, 'days')) {
+	
+	 centerday = m.format('YYYY-MM-DD')
+
+  
+
+	for (var num = 0; num < 30; num++) {
+		craw.queue({
+			uri: customSearch(centerday, num)
+		});
+	}
+
+}
